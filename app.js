dec6b94ca8eca0200d7625b5d692644008036a41
Daniel Antunes de Almeida <62892836+dan-dealmeida@users.noreply.github.com>
2023-12-10 19:34:41 -0300

    Update and fix errors on app.js

--- app.js
+++ app.js
@@ -8,63 +8,51 @@ const installDatabase = require('./middleware/dbInstaller');
 const User = require('./models/User');
 
 const app = express();
+const PORT = 3000;
+const mongoURI = 'mongodb+srv://admin:admin@cluster0.wb1rayu.mongodb.net/API-Daniel?retryWrites=true&w=majority';
 
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/favorites', favoritesRoutes);
-app.use('/realEstate', visitRoutes);
-app.use('/favorites', realEstateRoutes);
+app.use('/realEstate', realEstateRoutes);
+app.use('/visits', visitRoutes); 
 
-const PORT = 3000;
+mongoose.set('strictQuery', false);
 
-// Defina a rota GET /install/
-app.get('/install/', async (req, res) => {
-  try {
-    await mongoose.connect('mongodb+srv://admin:admin@cluster0.wb1rayu.mongodb.net/API-Daniel?retryWrites=true&w=majority');
-    await installDatabase();
-    res.status(200).json({ message: 'Instalação do banco de dados concluída com sucesso!' });
-  } catch (error) {
-    console.error('Erro ao instalar banco de dados:', error);
-    res.status(500).json({ error: 'Erro ao instalar banco de dados' });
-  }
-});
+mongoose.connect(mongoURI)
+  .then(async () => {
+    console.log('Connected to MongoDB');
+    await installDatabase(); 
 
+    // Verifying and creating default admin user
+    const createAdminUser = async () => {
+      try {
+        const adminUser = await User.findOne({ role: 'admin' });
 
+        if (!adminUser) {
+          const newAdmin = new User({
+            nome: 'Admin',
+            email: 'admin@example.com',
+            senha: 'senha123',
+            role: 'admin',
+          });
 
-mongoose.set("strictQuery", false)
-mongoose.
-connect('mongodb+srv://admin:admin@cluster0.wb1rayu.mongodb.net/API-Daniel?retryWrites=true&w=majority')
-.then(() => {
-    console.log('conectado ao MongoDB')
+          await newAdmin.save();
+          console.log('Default admin user created successfully!');
+        }
+      } catch (error) {
+        console.error('Error creating admin user:', error);
+      }
+    };
+
+    await createAdminUser();
+  })
+  .then(() => {
     app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
-}).catch((error) => {
-    console.log(error)
-})
-
-
-// Verificação e criação do usuário administrador padrão
-const createAdminUser = async () => {
-  try {
-    const adminUser = await User.findOne({ role: 'admin' });
-
-    if (!adminUser) {
-      const newAdmin = new User({
-        nome: 'Admin',
-        email: 'admin@example.com',
-        senha: 'senha123', 
-        role: 'admin',
-      });
-
-      await newAdmin.save();
-      console.log('Usuário administrador padrão criado com sucesso!');
-    }
-  } catch (error) {
-    console.error('Erro ao criar usuário administrador:', error);
-  }
-};
-
-createAdminUser().then(() => {
-  mongoose.connection.close();
-}
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exiting the process on DB connection error
+  });
